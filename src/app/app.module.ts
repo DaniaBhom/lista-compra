4872a840ea457ad87d7a08f55366599129a002c7
Daniela Bom <db.inventiva@gmail.com>
2022-06-09 16:27:43 -0300

    update angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { FormsModule } from '@angular/Forms';
+import { FormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './views/home/home.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
-import { EditmercadoriaComponent } from './editmercadoria/editmercadoria.component';
+
 
 
 @NgModule({
@@ -18,12 +17,11 @@ import { EditmercadoriaComponent } from './editmercadoria/editmercadoria.compone
     HomeComponent,
     HeaderComponent,
     FooterComponent,
-    EditmercadoriaComponent
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
     MatToolbarModule,
     FormsModule
   ],
